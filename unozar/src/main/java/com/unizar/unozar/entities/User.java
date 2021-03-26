a61439bc7299d02207cb4a48c4d51f2c32b12bd7
evilpicaporte <48834236+evilpicaporte@users.noreply.github.com>
2021-03-26 12:00:43 +0100

    Clase User terminada

--- unozar/src/main/java/com/unizar/unozar/entities/User.java
+++ unozar/src/main/java/com/unizar/unozar/entities/User.java
@@ -1,5 +1,10 @@
 package com.unizar.unozar.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Entity
 @Table(name = "USER")
 public class User {
@@ -26,16 +31,30 @@ public class User {
   @Column(name = "PUBLIC_TOTAL")
   private int public_total;
 
-  public User(String alias, String email, String password){
-    this.alias = alias;
-    this.email = email;
-    this.password = password;
-    this.private_wins = 0;
-    this.private_total = 0;
-    this.public_wins = 0;
-    this.public_total = 0;
+  // Increments the number of public games won
+  public void addPublicWin(){
+    public_wins++;
   }
 
+  // Increments the number of public games played
+  public void addPublicTotal(){
+    public_total++;
+  }
+
+  // Increments the number of private games won
+  public void addPrivateWin(){
+    private_wins++;
+  }
+
+  // Increments the number of private games played
+  public void addPrivateTotal(){
+    private_total++;
+  }
+
+  /////////////////////////
+  // Getters and Setters //
+  /////////////////////////
+  
   public String getAlias(){
     return alias;
   }
@@ -48,6 +67,22 @@ public class User {
     return password;
   }
 
+  public int getPublicWins(){
+    return public_wins;
+  }
+
+  public int getPublicTotal(){
+    return public_total;
+  }
+
+  public int getPrivateWins(){
+    return private_wins;
+  }
+
+  public int getPrivateTotal(){
+    return private_total;
+  }
+
   public void setAlias(String alias){
     this.alias = alias;
   }
@@ -58,5 +93,21 @@ public class User {
 
   public void setPassword(String password){
     this.password = password;
+  }
+
+  public void setPublicWins(int public_wins){
+    this.public_wins = public_wins;
+  }
+
+  public void setPublicTotal(int public_total){
+    this.public_total = public_total;
+  }
+
+  public void setPrivateWins(int private_wins){
+    this.private_wins = private_wins;
+  }
+
+  public void setPrivateTotal(int private_total){
+    this.private_total = private_total;
   }
 }
