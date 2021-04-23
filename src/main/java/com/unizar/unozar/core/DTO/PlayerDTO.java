0f66c692e7447b597197ff68fcd480421484ebd7
evilpicaporte <48834236+evilpicaporte@users.noreply.github.com>
2021-04-23 17:42:03 +0200

    DTO fix a JSON

--- src/main/java/com/unizar/unozar/core/DTO/PlayerDTO.java
+++ src/main/java/com/unizar/unozar/core/DTO/PlayerDTO.java
@@ -1,21 +1,29 @@
 package com.unizar.unozar.core.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.unizar.unozar.core.entities.Player;
 
 public class PlayerDTO{
   
+  @JsonProperty
   public String id;
   
+  @JsonProperty
   public String email;
   
+  @JsonProperty
   public String alias;
   
+  @JsonProperty
   public int private_wins;
   
+  @JsonProperty
   public int private_total;
   
+  @JsonProperty
   public int public_wins;
   
+  @JsonProperty
   public int public_total;
 
   public PlayerDTO(Player toTransfer){
