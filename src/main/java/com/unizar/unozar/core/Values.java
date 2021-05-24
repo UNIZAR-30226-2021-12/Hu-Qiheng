efd1133fdbe9efc2f3126fbeb9815a94bdd5e976
bqbit <57493992+bqbit@users.noreply.github.com>
2021-05-24 19:46:22 +0200

    Cambio en el número de desbloqueables

--- src/main/java/com/unizar/unozar/core/Values.java
+++ src/main/java/com/unizar/unozar/core/Values.java
@@ -8,10 +8,9 @@ public class Values{
   public final static int CARD_PRICE = 750;
   // Made so it can be changed the number of unlockables and their price
   public final static int A_SHOP[] = {0, 0, AVATAR_PRICE, AVATAR_PRICE,  
-                                      AVATAR_PRICE, AVATAR_PRICE, AVATAR_PRICE, 
                                       AVATAR_PRICE};
   public final static int B_SHOP[] = {0, BOARD_PRICE, BOARD_PRICE};
-  public final static int C_SHOP[] = {0, CARD_PRICE, CARD_PRICE};
+  public final static int C_SHOP[] = {0, CARD_PRICE, CARD_PRICE, CARD_PRICE};
 
   public final static int GIFTS[] = {10, 20, 30, 50, 100};
   
